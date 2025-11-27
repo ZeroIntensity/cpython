10903669140ee8285a2fb3c0df6e4166980f783c
Peter Bierma <zintensitydev@gmail.com>
2025-11-26 22:11:54 -0500

    Fix the test.

--- Lib/test/test_interpreters/test_object_proxy.py
+++ Lib/test/test_interpreters/test_object_proxy.py
@@ -5,7 +5,9 @@ from test.support import threading_helper
 # Raise SkipTest if subinterpreters not supported.
 import_helper.import_module('_interpreters')
 from concurrent.interpreters import share, SharedObjectProxy
-from .utils import TestBase
+from test.test_interpreters.utils import TestBase
+from threading import Barrier, Thread
+from concurrent import interpreters
 
 
 class SharedObjectProxyTests(TestBase):
@@ -22,7 +24,7 @@ class SharedObjectProxyTests(TestBase):
 
         # Shareable objects should pass through
         for shareable in (None, True, False, 100, 10000, "hello", b"world", memoryview(b"test")):
-            self.assertTrue(shareable)
+            self.assertTrue(interpreters.is_shareable(shareable))
             with self.subTest(shareable=shareable):
                 not_a_proxy = share(shareable)
                 self.assertNotIsInstance(not_a_proxy, SharedObjectProxy)
@@ -30,13 +32,22 @@ class SharedObjectProxyTests(TestBase):
 
     @threading_helper.requires_working_threading()
     def test_create_concurrently(self):
+        barrier = Barrier(4)
         def thread():
+            interp = interpreters.create()
+            barrier.wait()
             for iteration in range(100):
                 with self.subTest(iteration=iteration):
-                    self.test_create()
+                    interp.exec("""if True:
+                    from concurrent.interpreters import share
+                    import os
+
+                    unshareable = open(os.devnull)
+                    proxy = share(unshareable)
+                    unshareable.close()""")
 
         with threading_helper.catch_threading_exception() as cm:
-            with threading_helper.start_threads((thread for _ in range(4))):
+            with threading_helper.start_threads((Thread(target=thread) for _ in range(4))):
                 pass
 
             if cm.exc_value is not None:
@@ -44,5 +55,4 @@ class SharedObjectProxyTests(TestBase):
 
 
 if __name__ == '__main__':
-    # Test needs to be a package, so we can do relative imports.
     unittest.main()
